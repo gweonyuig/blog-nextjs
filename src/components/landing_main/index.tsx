accc016ae792932f872b020b2e240cfa0b63a71a
gweonyuig <gweonyuig@gmail.com>
2025-04-02 20:57:21 +0900

    refactor: data json파일로 분류 server action추가

--- src/components/landing_main/index.tsx
+++ src/components/landing_main/index.tsx
@@ -1,60 +1,47 @@
-import React from "react";
 import styles from "./styles.module.css";
 import Image from "next/image";
 import ProfileInfo from "../profile_info";
+import { getAboutMe } from "@/app/actions";
 
-const aboutMe = [
-  { title: "Name", description: "권유익", iconImg: "/icons/icon_name.svg" },
-  {
-    title: "Phone",
-    description: "010-1234-5678",
-    iconImg: "/icons/icon_phone.svg",
-  },
-  {
-    title: "Email",
-    description: "gweonyuig@gmail.com",
-    iconImg: "/icons/icon_email.svg",
-  },
-  {
-    title: "Birthday",
-    description: "1997-08-01",
-    iconImg: "/icons/icon_calendar.svg",
-  },
-  {
-    title: "Location",
-    description: "Cheonan, South Korea",
-    iconImg: "/icons/icon_location.svg",
-  },
-  {
-    title: "University",
-    description: "Sunmoon University",
-    iconImg: "/icons/icon_univer.svg",
-  },
-];
+// 타입 정의 추가
+interface AboutMeItem {
+  title: string;
+  description: string;
+  iconImg: string;
+}
 
-const LandingMain = () => (
-  <main className={styles.main}>
-    <Image
-      className={styles.profileImage}
-      src="/bualapha.png"
-      alt="no-image"
-      width={250}
-      height={250}
-    />
-    <div>
-      <h1 className={styles.headerTitle}>Information</h1>
-      <div className={styles.description}>
-        {aboutMe.map((item, index) => (
-          <ProfileInfo
-            key={index}
-            title={item.title}
-            description={item.description}
-            iconImg={item.iconImg}
-          />
-        ))}
+interface AboutMeResponse {
+  aboutMe: AboutMeItem[];
+}
+
+const LandingMain = async () => {
+  // 데이터 구조에 맞게 접근을 수정
+  const response: AboutMeResponse = await getAboutMe();
+  const aboutMeItems = response.aboutMe;
+
+  return (
+    <main className={styles.main}>
+      <Image
+        className={styles.profileImage}
+        src="/bualapha.png"
+        alt="no-image"
+        width={250}
+        height={250}
+      />
+      <div>
+        <h1 className={styles.headerTitle}>Information</h1>
+        <div className={styles.description}>
+          {aboutMeItems.map((item, index) => (
+            <ProfileInfo
+              key={index}
+              title={item.title}
+              description={item.description}
+              iconImg={item.iconImg}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </main>
-);
-
+    </main>
+  );
+};
 export default LandingMain;
