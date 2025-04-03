ac622aae92088e0ff0bd46a89cf72ed2f15c7093
gweonyuig <gweonyuig@gmail.com>
2025-04-03 22:13:40 +0900

    feat: main-page-blog-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from "./page.module.css";
 import LandingInfo from "@/components/landing_info";
 import LandingProject from "@/components/landing_project";
+import LandingBlog from "@/components/landing_blog";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <LandingInfo />
       <LandingProject />
+      <LandingBlog />
     </div>
   );
 }
